00ea78973501b4bd4050ec34b4792c2251a318dd
Christopher Ras <c.ras@trancon.nl>
2018-09-24 16:19:34 +0200

    mod 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -41,6 +41,28 @@ namespace GradeBook.GradeBooks
             }
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.Write("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.Write("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
+
         public List<double> SortedLetterGrades()
         {
             List<double> unSortedStudents = new List<double>();
